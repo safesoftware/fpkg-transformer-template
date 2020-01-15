8ac3b2b2bd66f4618c3b67200fba9c1f1c3206cf
Heather Li <heather.li@safe.com>
2020-01-14 16:54:36 -0800

    refactor parameter validation

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,31 +6,28 @@ import re
 import sys
 
 
+def validate_parameter(parameter, regex_pattern, parameter_value):
+    if not re.match(regex_pattern, parameter_value):
+        print('{name} needs to match {pattern}'.format(name=parameter, pattern=regex_pattern))
+        return False
+    return True
+
+
 failed = False
-
-
-def print2(msg):
-    global failed
-    failed = True
-    print(msg)
-
 
 # UID pattern from FME Packages Specification.
 UID_PATTERN = r'^[a-z0-9](?!.*--)[a-z0-9-]{1,30}[a-z0-9]$'
-if not re.match(UID_PATTERN, '{{cookiecutter.publisher_uid}}'):
-    print2('Publisher UID needs to match ' + UID_PATTERN)
-if not re.match(UID_PATTERN, '{{cookiecutter.package_uid}}'):
-    print2('Package UID needs to match ' + UID_PATTERN)
-
+failed &= validate_parameter('Package UID', UID_PATTERN, '{{cookiecutter.package_uid}}')
+failed &= validate_parameter('Publisher UID', UID_PATTERN, '{{cookiecutter.publisher_uid}}')
 
 TRANSFORMER_PATTERN = r'^[A-Za-z0-9]+$'
-if not re.match(TRANSFORMER_PATTERN, '{{cookiecutter.transformer_name}}'):
-    print2('Transformer name needs to match ' + TRANSFORMER_PATTERN)
+failed &= validate_parameter('Transformer name', TRANSFORMER_PATTERN,
+                             '{{cookiecutter.transformer_name}}')
 
 
 PYTHON_MODULE_PATTERN = r'^[A-Za-z0-9_]+$'
-if not re.match(PYTHON_MODULE_PATTERN, '{{cookiecutter.python_module}}'):
-    print2('Python module name needs to match ' + PYTHON_MODULE_PATTERN)
+failed &= validate_parameter('Python module name', PYTHON_MODULE_PATTERN,
+                             '{{cookiecutter.python_module}}')
 
 
 if failed:
