78775a20e7b7a3d1cc2e4fb411592e368448f52d
Carson Lam <carson.lam@safe.com>
2022-02-16 16:21:57 -0800

    Expand doc in main py file.

--- {{cookiecutter.package_uid}}/python/{{cookiecutter.python_package}}/src/{{cookiecutter.python_module}}/{{cookiecutter.transformer_file_name}}.py
+++ {{cookiecutter.package_uid}}/python/{{cookiecutter.python_package}}/src/{{cookiecutter.python_module}}/{{cookiecutter.transformer_file_name}}.py
@@ -1,26 +1,35 @@
 """
-{{cookiecutter.publisher_uid}}.{{cookiecutter.package_uid}}.{{cookiecutter.transformer_name}}
-Python implementation.
-
-The Python package's setup.py is configured such that
-the Python code for the transformer must be entirely in this file.
+{{cookiecutter.publisher_uid}}.{{cookiecutter.package_uid}}.{{cookiecutter.transformer_name}} implementation.
 """
+from fmeobjects import FMEFeature
 
 
-class {{cookiecutter.transformer_name}}(object):
+class {{cookiecutter.transformer_name}}:
     """
-    One instance of this class is created per instance of the transformer in the workspace.
+    Each instance of the transformer in the workspace has an instance of this class.
     All instances are created at the beginning of the workspace translation.
     """
 
     def __init__(self):
         pass
 
-    def input(self, feature):
-        first_name = feature.getAttribute('__xformer_first_name')
-        feature.setAttribute('_greeting', 'Hello, {}!'.format(first_name))
+    def input(self, feature: FMEFeature):
+        """
+        Receive an input feature.
+        """
+        # The FMX file supplies all parameters as attributes on the input feature.
+        # After getting the values of these attributes, they need to be removed,
+        # so they don't show up on output features.
+        first_name = feature.getAttribute("__xformer_first_name")
+        feature.removeAttribute("__xformer_first_name")
+
+        # Set the output attribute, and output the feature.
+        feature.setAttribute("_greeting", "Hello, {}!".format(first_name))
         self.pyoutput(feature)
 
-    def pyoutput(self, feature):
+    def pyoutput(self, feature: FMEFeature):
+        """
+        Emit an output feature.
+        """
         # Implementation is injected by FME.
         pass
