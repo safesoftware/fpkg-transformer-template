a09c03086b34c4e64ae52436558e55b435d7b3d3
Carson Lam <carson.lam@safe.com>
2022-02-17 17:09:31 -0800

    Add pytest example.

---
+++ {{cookiecutter.package_uid}}/python/{{cookiecutter.python_module}}/tests/test_{{cookiecutter.python_filename}}.py
@@ -0,0 +1,38 @@
+from {{cookiecutter.python_module}}.{{cookiecutter.python_filename}} import {{cookiecutter.transformer_name}}
+from fmeobjects import FMEFeature
+
+
+class MockTransformer({{cookiecutter.transformer_name}}):
+    """
+    This is a subclass of the transformer under test,
+    but with its pyoutput() features saved to an out_features member,
+    where they can be recovered for validation.
+    """
+    def __init__(self):
+        super().__init__()
+        self.out_features = []
+
+    def pyoutput(self, feature):
+        self.out_features.append(feature)
+
+
+def test_greeting():
+    """
+    Verify the value of the greeting attribute on the output feature
+    based on a given input feature.
+    """
+    # Build an input feature with internal attribute for parameter.
+    in_feature = FMEFeature()
+    in_feature.setAttribute("__xformer_first_name", "World")
+
+    # Create the transformer and pass it the input feature.
+    transformer = MockTransformer()
+    transformer.input(in_feature)
+
+    # There should be only 1 output feature,
+    # with the expected greeting attribute and value.
+    # The internal attribute should not be present.
+    assert len(transformer.out_features) == 1
+    out_feature = transformer.out_features[0]
+    assert out_feature.getAttribute("_greeting") == "Hello, World!"
+    assert out_feature.isAttributeMissing("__xformer_first_name")
